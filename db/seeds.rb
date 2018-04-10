86d47a0fe432aaf290063c2f004a631ee696f9e6
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-10 17:28:46 -0400

    lab completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Daft Punk").songs << Song.create(title: "The Grid")
